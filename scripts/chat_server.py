9d7f519bc265b4168df255106e9cceb5c4e61d54
Hampus Dunström <hampusdunstrom@gmail.com>
2017-02-09 11:50:56 +0100

    wrote a chat server

---
+++ scripts/chat_server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Message
+
+new_messages = list()
+
+
+def callback(data):
+    new_messages.append((data.sender, data.message))
+
+
+def run_server():
+    global new_messages
+    pub = rospy.Publisher('chat_out', Message, queue_size=10)
+    rospy.init_node('chat_server', anonymous=True)
+    rospy.Subscriber('chat_in', Message, callback)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        messages = new_messages
+        new_messages = list()
+        for message in messages:
+            pub.publish(sender=message.sender, message=message.message)
+        rate.sleep()
+
+if __name__ == "__main__":
+    try:
+        run_server()
+    except rospy.ROSInterruptException:
+        pass
