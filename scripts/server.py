ef727a8de77238a75c73f52d18f05858306517d3
Hampus Dunström <hampusdunstrom@gmail.com>
2017-02-16 14:00:26 +0100

    added a service

--- scripts/server.py
+++ scripts/server.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 import rospy
 from chat_server.msg import Message
+from chat_server.srv import NmbClients
 
 new_messages = list()
+pub = None
 
 
 def callback(data):
     new_messages.append((data.sender, data.message))
 
 
+def get_nmbr_of_clients(req):
+    global pub
+    return NmbClientsResponse(pub.get_num_connections())
+
+
 def run_server():
     global new_messages
+    global pub
     pub = rospy.Publisher('chat_out', Message, queue_size=10)
     rospy.init_node('chat_server', anonymous=False)
     rospy.Subscriber('chat_in', Message, callback)
+    s = rospy.Service('nmb_of_clients', NmbClients, get_nmbr_of_clients)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         messages = new_messages
